c2bb9c9734417a3a987267fdc079b6f69b486203
carbingus <exthalia2015@gmail.com>
2023-05-20 17:35:21 -0300

    agregados metodos iniciales

--- src/test_ulp/AccesoADatos/MateriaData.java
+++ src/test_ulp/AccesoADatos/MateriaData.java
@@ -9,11 +9,35 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
-import test_ulp.Entidades.Alumnos; 
+import test_ulp.Entidades.*;
 
 public class MateriaData {
-    
-    private Connection con = null;
-    
-    
+
+    private Connection con;
+
+    private Conexion conexion;
+
+    public MateriaData(Conexion conexion) {
+    }
+
+    public void guardarMateria(Materia materia) {
+    }
+
+    public Materia buscarMateria(int id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Materia modificarMateria(int id, Materia materia) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void eliminarMateria(int id) {
+    }
+
+    public List<Materia> listarMaterias() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void activarMateria(int id) {
+    }
 }
