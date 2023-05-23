b6c050a1c0ba995287a641976546395aa09a9bfd
kevinhuanca <kevinhuanca75@gmail.com>
2023-05-23 17:05:35 -0300

    Prueba DB

--- src/test_ulp/Test_ulp.java
+++ src/test_ulp/Test_ulp.java
@@ -1,9 +1,14 @@
 package test_ulp;
 
+import test_ulp.AccesoADatos.MateriaData;
+import test_ulp.Entidades.Materia;
+
 public class Test_ulp {
 
     public static void main(String[] args) {
-        
+        MateriaData md = new MateriaData();
+        Materia m =  new Materia("Programacion 1", 1, true);
+        md.guardarMateria(m);
     }
     
 }
