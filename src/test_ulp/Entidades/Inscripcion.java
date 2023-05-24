3128317ccf03dc41278cabe3acb578d936a21b61
carbingus <exthalia2015@gmail.com>
2023-05-24 14:54:27 -0300

    cambios en codigo y mejoras

--- src/test_ulp/Entidades/Inscripcion.java
+++ src/test_ulp/Entidades/Inscripcion.java
@@ -1,40 +1,22 @@
 package test_ulp.Entidades;
 
 public class Inscripcion {
+    
     private int id_inscripto;
     private double nota;
     private Alumno alumno;
     private Materia materia;
 
-    public Inscripcion() {
-    }
-
-    public Inscripcion(double nota, Alumno alumno, Materia materia) {
-        this.nota = nota;
+    public Inscripcion(int id_inscripto, Alumno alumno, Materia materia, double nota){
+        this.id_inscripto = id_inscripto;
         this.alumno = alumno;
         this.materia = materia;
+        this.nota = nota;
     }
     
-    public Inscripcion(int id_inscripto, double nota, Alumno alumno, Materia materia){
-        this.id_inscripto = id_inscripto;
-        this.nota = nota;
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
         this.alumno = alumno;
         this.materia = materia;
-    }
-
-    public int getId_inscripto() {
-        return id_inscripto;
-    }
-
-    public void setId_inscripto(int id_inscripto) {
-        this.id_inscripto = id_inscripto;
-    }
-
-    public double getNota() {
-        return nota;
-    }
-
-    public void setNota(double nota) {
         this.nota = nota;
     }
 
@@ -54,4 +36,42 @@ public class Inscripcion {
         this.materia = materia;
     }
     
+    public Inscripcion(){
+    }
+    
+    public Inscripcion(double nota){
+        this.nota = nota;
+    }
+    
+    public int getIdInscripto(){
+        return id_inscripto;
+    }
+    
+    public void setIdInscripto(int id_inscripto){
+        this.id_inscripto = id_inscripto;
+    }
+    
+    public int getIdAlumno(){
+        return alumno.getId_alumno();
+    }
+    
+    public void setIdAlumno(int id_alumno){
+        this.alumno.setId_alumno(id_alumno);
+    }
+    
+    public int getIdMateria(){
+        return materia.getId_materia();
+    }
+    
+    public void setIdMateria(int id_materia){
+        this.materia.setId_materia(id_materia);    
+    }
+    
+    public double getNota(){
+        return nota;
+    }
+    
+    public void setNota(double nota){
+        this.nota = nota;
+    }
 }
