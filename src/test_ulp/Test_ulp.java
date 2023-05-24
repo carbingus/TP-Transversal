1091600338ca07266ab2a183816ece2fd379edc5
carbingus <exthalia2015@gmail.com>
2023-05-24 18:53:01 -0300

    Update Test_ulp.java

--- src/test_ulp/Test_ulp.java
+++ src/test_ulp/Test_ulp.java
@@ -7,9 +7,26 @@ import test_ulp.Entidades.Materia;
 public class Test_ulp {
 
     public static void main(String[] args) {
-        MateriaData md = new MateriaData();
-//        Materia m =  new Materia("Programacion 1", 1, true);
-//        md.guardarMateria(m);
+        MateriaData md = new MateriaData(); //generamos un nuevo MateriaData
+        
+        
+        //creamos una nueva materia y le asignamos informacion respectiva
+        //Materia("nombre_materia",anio, estado_materia)
+        //anio indica el año de cursada, primer año, segundo año, utilizar solo numeros
+        //estado utiliza un boolean true/false, para indicar que esta activa o no
+        //el estado en la base de datos aparece como "TinyInt"
+        //ya que TinyInt es la forma de utilizar booleans
+        //por lo tanto si estado_materia = true ó si estado_materia = 1
+        //la materia es considerada como activa
+        //de la misma manera, si estado_materia = false ó si estado_materia = 0
+        //la materia es considerada como inactiva
+        
+         Materia m =  new Materia("Programacion 1", 1, true); 
+         md.guardarMateria(m); // con este metodo, se guarda la materia en la base de datos
+         
+         //el siguiente metodo sirve para verificar si la materia existe o no, dentro de la base de datos
+         //utilizando md.buscarMateria(id_materia) como parametro para realizar la busqueda
+         //en este caso ejemplo, la nueva materia tendra id 9
         /*
         if (md.buscarMateria(9) == null) {
             JOptionPane.showMessageDialog(null, "Materia NO encontrada!");
@@ -18,14 +35,18 @@ public class Test_ulp {
         }
         */
         
-//        md.modificarMateria(new Materia(13, "Programacion 2", 3, true));
+//        md.modificarMateria(new Materia(13, "Programacion 2", 3, true)); //aca modificamos la info de la materia
         
-//        md.eliminarMateria(10);
+//        md.eliminarMateria(10); //aca eliminamos la materia buscandola por id_materia (en este caso su id es 10)
 
+        //este loop nos lista por consola, todas las materias que tengamos dentro de la base de datos
         for (Materia materia : md.listarMaterias()) {
             System.out.println(materia.toString());
             System.out.println("");
         }
+        
+        //faltaria luego agregar alumnos usando los metodos de AlumnoData
+        //y finalmente inscribir alumnos a materias, utilizando InscripcionData
         
     }
     
