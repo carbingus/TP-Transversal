d3a3cba6c13923906ac5aa98e7c7c06ae073235a
carbingus <exthalia2015@gmail.com>
2023-05-24 14:54:54 -0300

    implementacion de metodo para inscribir y obtener inscriptos

--- src/test_ulp/AccesoADatos/InscripcionData.java
+++ src/test_ulp/AccesoADatos/InscripcionData.java
@@ -4,8 +4,8 @@
  */
 package test_ulp.AccesoADatos;
 
+import test_ulp.Entidades.*;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,26 +13,66 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
-import test_ulp.Entidades.*;
 
 
 public class InscripcionData {
 
-    private Connection con;
-
-    private Conexion conexion;
-
-    public InscripcionData(Conexion conexion) {
+    private Connection con = null;
+    private MateriaData matData = new MateriaData();
+    private AlumnoData alumData = new AlumnoData();
+//    private Conexion conexion;
+    
+    public InscripcionData(Conexion conexion){
+        this.con = Conexion.getConexion();
     }
-
-    public void inscribir(Inscripcion insc) {
+    
+    private void InscribirAlumno(Inscripcion insc){
+        String sql = "INSERT INTO inscripcion(id_alumno, id_materia, nota) VALUES (?, ?, ?)";
+        try{
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, insc.getIdAlumno());
+            ps.setInt(2, insc.getIdMateria());
+            ps.setDouble(3, insc.getNota());
+            
+           ps.executeUpdate();
+           ResultSet rs = ps.getGeneratedKeys();
+            
+            if (rs.next()){
+                insc.setIdInscripto(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "La inscripcion se ha realizado correctamente.");
+            }
+        } catch (SQLException ex){
+            
+        
+                JOptionPane.showMessageDialog(null, "Error al acceder a la Tabla Inscripcion "+ex.getMessage());
+        }
     }
+    
+    public List<Inscripcion> obtenerInscripciones(){
+        List<Inscripcion> cursadas = new ArrayList<>();
+        try{
+            String sql = "SELECT * FROM inscripcion;";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            Inscripcion insc;
+            
+            while(rs.next()){
+                insc = new Inscripcion();
+                insc.setIdInscripto(rs.getInt("id_inscripto"));
+                
+                Alumno a = alumData.buscarAlumno(rs.getInt("id_alumno"));
+                insc.setIdAlumno(a.getId_alumno());
+                
+                Materia m = matData.buscarMateria(rs.getInt("id_materia"));
+                insc.setIdMateria(m.getId_materia());
+                insc.setNota(rs.getDouble("nota"));
 
-    public Alumno buscarAlumno(int id) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    public Materia buscarMateria(int id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+                cursadas.add(insc);
+            }      
+            ps.close();
+        }catch (SQLException ex) {
+            JOptionPane.showInternalMessageDialog(null, "Error al realizar la inscripcion: " +ex.getMessage());
+        }
+        return cursadas;
     }
 }
