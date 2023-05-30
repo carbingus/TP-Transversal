c5a00ab0706e99a2443866c888f1f4960d6bf51b
carbingus <exthalia2015@gmail.com>
2023-05-30 12:25:46 -0300

    implementacion de codigo inicial para FormularioNotas

--- src/test_ulp/Vistas/FormularioNotas.java
+++ src/test_ulp/Vistas/FormularioNotas.java
@@ -1,66 +1,189 @@
 package test_ulp.Vistas;
 
+import test_ulp.Entidades.*;
+import test_ulp.AccesoADatos.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
 public class FormularioNotas extends javax.swing.JInternalFrame {
+    
+    private List<Alumno> alumnos;
+    private AlumnoData alumData = new AlumnoData();
+    private List<Inscripcion> inscripcion;
+    private InscripcionData inscrData = new InscripcionData();
 
     public FormularioNotas() {
         initComponents();
+        cargarListaAlumnosCBox();
+        
+    }
+    
+    private void cargarListaAlumnosCBox(){
+        cboxAlumno.removeAllItems();
+       
+        this.alumnos = alumData.listarAlumnos();
+
+        for (Alumno a1u : alumnos) {
+
+            cboxAlumno.addItem(a1u);
+
+        }
+    }
+        
+    private void agregarAlumno(){
+        cboxAlumno.removeAll();
+        this.alumnos = (ArrayList<Alumno>) alumData.listarAlumnos();
+
+        for (Alumno alu : alumnos){
+            cboxAlumno.addItem(alu);
+        }
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
+        jbtnGuardar = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        cboxAlumno = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jtblAlum = new javax.swing.JTable();
+        jbtnSalir = new javax.swing.JButton();
 
         setClosable(true);
 
-        jButton1.setText("jButton1");
+        jbtnGuardar.setText("Guardar");
+        jbtnGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnGuardarActionPerformed(evt);
+            }
+        });
 
         jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         jLabel1.setText("Carga de Notas");
 
-        jLabel2.setText("jLabel2");
+        jLabel2.setText("Seleccione un alumno");
+
+        cboxAlumno.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cboxAlumnoActionPerformed(evt);
+            }
+        });
+
+        jtblAlum.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
+            },
+            new String [] {
+                "Codigo", "Nombre", "Nota"
+            }
+        ));
+        jScrollPane1.setViewportView(jtblAlum);
+
+        jbtnSalir.setText("Salir");
+        jbtnSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnSalirActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(33, 33, 33))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(123, 123, 123)
+                        .addGap(124, 124, 124)
                         .addComponent(jLabel1))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(65, 65, 65)
-                        .addComponent(jLabel2)))
-                .addContainerGap(138, Short.MAX_VALUE))
+                        .addGap(115, 115, 115)
+                        .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(70, 70, 70)
+                .addComponent(jbtnGuardar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jbtnSalir)
+                .addGap(70, 70, 70))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(20, Short.MAX_VALUE)
+                        .addComponent(jLabel2)
+                        .addGap(95, 95, 95)
+                        .addComponent(cboxAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addGap(26, 26, 26))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(23, 23, 23)
+                .addContainerGap()
                 .addComponent(jLabel1)
-                .addGap(37, 37, 37)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 120, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(22, 22, 22))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(cboxAlumno, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(23, 23, 23)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 256, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jbtnGuardar)
+                    .addComponent(jbtnSalir))
+                .addContainerGap(45, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void cboxAlumnoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboxAlumnoActionPerformed
+
+        inscripcion = inscrData.obtenerInscripcionesPorAlumno(((Alumno) cboxAlumno.getSelectedItem()).getId_alumno());
+
+        DefaultTableModel model = (DefaultTableModel) jtblAlum.getModel();
+        model.setRowCount(0);
+
+        if (!inscripcion.isEmpty()) {
+            for (Inscripcion i1 : inscripcion) {
+
+                model.addRow(new Object[]{i1.getMateria().getId_materia(), i1.getMateria().getNombre_materia(), i1.getNota()});
+
+            }
+        }
+    }//GEN-LAST:event_cboxAlumnoActionPerformed
+
+    private void jbtnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnGuardarActionPerformed
+        // TODO add your handling code here:
+        int i;
+        for (i = 0 ; i < jtblAlum.getRowCount(); i++){
+            inscrData.actualizarNota(((Alumno) cboxAlumno.getSelectedItem()).getId_alumno(), Integer.parseInt(jtblAlum.getValueAt(i, 0).toString()), Double.parseDouble(jtblAlum.getValueAt(i,2).toString()));
+        }
+    }//GEN-LAST:event_jbtnGuardarActionPerformed
+
+    private void jbtnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnSalirActionPerformed
+        // TODO add your handling code here:
+        dispose();
+    }//GEN-LAST:event_jbtnSalirActionPerformed
+
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JComboBox<Alumno> cboxAlumno;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JButton jbtnGuardar;
+    private javax.swing.JButton jbtnSalir;
+    private javax.swing.JTable jtblAlum;
     // End of variables declaration//GEN-END:variables
 }
